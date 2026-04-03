a5f00139d3f9f720b2a60ba4836cf8471932dd0b
Lukas Brunner <l.brunner@students.htl-leonding.ac.at>
2026-04-03 12:15:45 +0200

    typed.js libary eingebunden und StartGame erstellt

--- project/js/script.js
+++ project/js/script.js
@@ -12,8 +12,8 @@ function backToStart(){
     document.body.innerHTML =
     `<div id="background">
         <div id="Logo"></div>
-        <div id="start-button"></div>
-        <div id="leaderboard-button"></div>
+        <div id="start-button" onclick="StartGame()"></div>
+        <div id="leaderboard-button" onclick="openLeaderBoard()"></div>
         <div id="howTo-button" onclick="openHowTo()"></div>
     </div>`;
 }
@@ -28,5 +28,40 @@ function openLeaderBoard(){
             </div>
         </div>
         <div id="back" onclick="backToStart()"></div>
+    </div>`;
+}
+
+function StartGame(){
+    document.body.innerHTML = 
+    `<div id="backgroundStart">
+        <div id="typedOutput"></div>
+    </div>`;
+
+    setTimeout(() => {
+        const typed = new Typed('#typedOutput', {
+            strings: ['Willkommen zu Web Escape!',
+                      'Du hast versucht dich in das System einzuloggen, aber es ist etwas schief gelaufen.',
+                      'Du bist in dem System gefangen und musst nun versuchen zu entkommen.',
+                      'Logge dich als Administrator ein, damit du Zugriff auf das System hast und es verlassen kannst.',
+                      'Aber das System wird dir nicht so einfach Zugriff gewähren.',
+                      'Viel Erfolg!'
+            ],
+            typeSpeed: 50,
+            backSpeed: 25,
+            backDelay: 2000,
+            startDelay: 1000,
+            loop: false,
+            showCursor: false
+        });
+}, 100);
+setTimeout(() => {
+    StartFirstLevel();
+}, 50000);
+}
+
+function StartFirstLevel(){
+    document.body.innerHTML = 
+    `<div id="backgroundFirstRoom">
+        <div id="firstLevel"></div>
     </div>`;
 }
