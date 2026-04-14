3365e06bf7529de2adf7a4abf742549a9eb3f50e
Lukas Brunner <l.brunner@students.htl-leonding.ac.at>
2026-04-14 15:17:17 +0200

    Add second level layout and background images for Room 2

--- project/js/Room2.js
+++ project/js/Room2.js
@@ -1,7 +1,13 @@
 function StartSecondLevel(){
     document.body.innerHTML = 
     `<div id="backgroundSecondRoom">
-        <div id="secondLevel"></div>
-        <div id="text">Welcome, Admin!</div>
+            <div id="container">
+                <div id="picture1"></div>
+                <div id="picture2"></div>
+                <div id="picture3"></div>
+                <div id="picture4"></div>
+                <div id="picture5"></div>
+                <div id="picture6"></div>
+            </div>
     </div>`;
 }
