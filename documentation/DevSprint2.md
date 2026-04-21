2cb06e8c27902063cae3efa86a4edc2af4394506
Lukas Brunner <l.brunner@students.htl-leonding.ac.at>
2026-04-21 15:34:37 +0200

    Implement structure for code changes with placeholders for future updates

---
+++ documentation/DevSprint2.md
@@ -0,0 +1,15 @@
+## Lukas Brunner | 2BHITM | WebEscape – Gefangen im Netz | https://github.com/htl-leo-medtwt-projects/2526-2bhitm-sommerprojekt-Brunner-Lukas
+
+## Änderungen
+
+- Room2
+    - kompletter funktionierender Raum
+- Raum 3
+    - das erste Rätsel gemacht mit Sticky Note
+    - das zweite Rätsel ohne sticky Note
+
+## Ziele
+
+- Raum 3 fertig machen
+- Raum 4 anfangen
+- Timer oben rechts machen
--- project/js/Room3.js
+++ project/js/Room3.js
@@ -1,4 +1,44 @@
 function StartThirdLevel(){
     document.body.innerHTML = `
-    <div>/div>`;
+    <div id="backgroundThirdRoom">
+    <div id="firstStickyNote" onclick="showFirstRiddleTip()"></div>
+    <div id="secondStickyNote" onclick="showSecondRiddleTip()"></div>
+    <div id="firstRiddle">
+    <h2>01001110</h2>
+    <h2>00110011</h2>
+    <h2>01010100</h2>
+    </div>
+    <div id="secondRiddle">
+    <h2>function unlockServer() {
+        let code = 1234
+        if (code == 5987) {
+            return "ACCESS GRANTED"
+        } else {
+            return "ACCESS DENIED"
+        }
+    }</h2>
+    </div>
+    </div>`;
 }
+
+function showFirstRiddleTip(){
+        document.body.innerHTML = `
+    <div id="backgroundThirdRoom">
+     <div id="StickyNoteTip1">
+        <div id="text">
+            <div id="closeTip" onclick="StartThirdLevel()">Back</div>
+            <h2>Hint: Binary code can be converted to text using an ASCII table.</h2>
+            <h2>You can convert binary to decimal by:
+
+            Multiplying each binary digit by 2 raised to its position (starting from 0 on the right), then adding the results.
+
+            Example:
+            1011₂ = (1×2³) + (0×2²) + (1×2¹) + (1×2⁰)
+            = 8 + 0 + 2 + 1 = 11₁₀</h2>
+            <h2>Use the ASCII table to find the corresponding characters for each binary code.</h2>
+        </div>
+    </div>
+    </div>`;
+}
+
+
